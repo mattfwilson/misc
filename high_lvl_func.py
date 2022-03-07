a0be9b367a2d3723345a9d226fcefc8db8a97ae4
mattfwilson <mattfwilson@gmail.com>
2022-03-07 03:20:58 -0500

    Trying out mp() higher level function

--- high_lvl_func.py
+++ high_lvl_func.py
@@ -1,14 +1,18 @@
 bills = [100, 57, 133, 89, 218]
 
-def add_tax(total):
-    tax = total * .06
-    new_total = total + tax
-    return new_total
+# def add_tax(total):
+#     tax = total * .06
+#     new_total = total + tax
+#     return new_total
 
-def add_tip(total):
-    tip = total * .20
-    new_total = total + tip
-    return new_total
+# def add_tip(total):
+#     tip = total * .20
+#     new_total = total + tip
+#     return new_total
 
+def double(num):
+    return num * 2
 
-
+doubled = map(double, bills)
+print(doubled)
+print(list(doubled))
