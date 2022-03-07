02e33b8592ede13975fef434ed8d336018e80eef
mattfwilson <mattfwilson@gmail.com>
2022-03-06 21:10:20 -0500

    Higher level functions test

---
+++ high_lvl_func.py
@@ -0,0 +1,14 @@
+bills = [100, 57, 133, 89, 218]
+
+def add_tax(total):
+    tax = total * .06
+    new_total = total + tax
+    return new_total
+
+def add_tip(total):
+    tip = total * .20
+    new_total = total + tip
+    return new_total
+
+
+
