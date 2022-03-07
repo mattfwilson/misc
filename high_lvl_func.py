5d579fd57ad928854dd54a8280c1c0e6c8331ab6
mattfwilson <mattfwilson@gmail.com>
2022-03-07 11:16:34 -0500

    More testing of higher level functions

--- high_lvl_func.py
+++ high_lvl_func.py
@@ -1,18 +1,27 @@
 bills = [100, 57, 133, 89, 218]
+new_bills = []
+names = ["Molly", "Matt", "Janelle", "Max", "Tim", "Colette", "Brian", "Amanda", "Shawn", "Katie"]
 
-# def add_tax(total):
-#     tax = total * .06
-#     new_total = total + tax
-#     return new_total
+def add_tax(total):
+    tax = total * .06
+    new_total = total + tax
+    return new_total
 
-# def add_tip(total):
-#     tip = total * .20
-#     new_total = total + tip
-#     return new_total
+def add_tip(total):
+    tip = total * .20
+    new_total = total + tip
+    return new_total
 
-def double(num):
-    return num * 2
+for i in range(len(bills)):
+  total = add_tip(bills[i])
+  new_bills.append("Total amount owed is $" + "{:.2f}".format(total) + ". Thank you!")
 
-doubled = map(double, bills)
-print(doubled)
-print(list(doubled))
+for x in new_bills:
+    print(x)
+
+for name in range(len(names)):
+    print(name)
+
+# doubled = map(lambda bill: bill * 2, bills)
+# print(doubled)
+# print(list(doubled))
