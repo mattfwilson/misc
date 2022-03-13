610c7a8d04e4398f7a8e1b24500b775e21768eed
mattfwilson <mattfwilson@gmail.com>
2022-03-13 17:49:44 -0400

    Got another function working totals_bills()

--- high_lvl_func.py
+++ high_lvl_func.py
@@ -22,9 +22,12 @@ def total_bills(func1, func2, func3, list):
     for i in range(len(list)):
         tip_and_tax = func1(list[i]) + func2(list[i])
         divided = func3(tip_and_tax)
-        total = i + tip_and_tax
+        total = i + divided 
         new_bills.append("Total amount owed is $" + "{:.2f}".format(total))
     return new_bills
+
+totz = total_bills(add_tax, add_tip, divide_by_four, bills)
+print(totz)
 
 a_in_name = filter(lambda name: "a" not in name and "l" in name, names)
 a_names = list(a_in_name)
