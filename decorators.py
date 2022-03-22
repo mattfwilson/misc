b90c94d47a660a145b8d350945fc75f68fa3b4e8
mattfwilson <mattfwilson@gmail.com>
2022-03-22 01:19:28 -0400

    Added in age to the decorated function

--- decorators.py
+++ decorators.py
@@ -5,7 +5,7 @@ def title_decorator(print_name_func):
     return wrapper
 
 @title_decorator
-def print_name(name):
-    print(name)
+def print_name(name, age):
+    print(F"{name} you are {age}!")
 
-print_name("Matt")
+print_name("Matt", 35)
