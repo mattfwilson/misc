e555a506f0a32b03d8b1304bc6e784723a179f5d
mattfwilson <mattfwilson@gmail.com>
2022-03-23 11:20:43 -0400

    Playing around with lambda functions

---
+++ lambda.py
@@ -0,0 +1,10 @@
+find_tip = lambda bill: bill * .06 + bill * .2
+print(find_tip(125))
+def boom(input):
+    uppercase = input.upper()
+    return print(uppercase)
+
+boom("hello")
+
+lambda_upper = lambda input: input.upper()
+print(lambda_upper)
