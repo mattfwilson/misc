0689a19c4056001cb233b68cbf2a90170e4f560a
mattfwilson <mattfwilson@gmail.com>
2022-03-24 11:30:24 -0400

    More lambda experiments

--- lambda.py
+++ lambda.py
@@ -8,3 +8,8 @@ boom("hello")
 
 lambda_upper = lambda input: input.upper()
 print(lambda_upper)
+
+
+names = ["mike", "att", "thry", "janelle", "hptp", "jkjk", "orly", "qwerty", "awsd"]
+no_vowels = filter(lambda name: name[0] == "a" or name[0] == "e" or name[0] == "i" or name[0] == "o" or name[0] == "u", names)
+print(list(no_vowels))
