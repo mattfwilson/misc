ff1de5b49998f59e94d4791c69f58d1ef65fe7c5
mattfwilson <mattfwilson@gmail.com>
2022-03-30 03:05:47 -0400

    Updating boom/uppercase function

--- lambda.py
+++ lambda.py
@@ -3,19 +3,22 @@ import functools
 find_tip = lambda bill: bill * .06 + bill * .2
 print(find_tip(125))
 
+boomed_words = []
+
 def boom(input):
     uppercase = input.upper()
-    return print(uppercase)
+    return boomed_words.append(uppercase)
 
 boom("hello")
+boom("lol")
+boom('elden ring')
+print(boomed_words)
 
 lambda_upper = lambda input: input.upper()
-print(lambda_upper)
-
 
 names = ["mike", "att", "thry", "jonelle", "hptp", "jkjk", "hope", "qwerty", "awsd"]
 no_vowels = filter(lambda name: name[0] == "a" or name == "jkjk" or name != "qwerty", names)
 print(list(no_vowels))
 
-lst = [8, 21, 5, 4, 783, 1]
-print(functools.reduce(lambda a, b: a if a > b else b, lst))
+lst = [8, 21, 5, -101, 4, 783, -51]
+print(functools.reduce(lambda a, b: a if a <= b else b, lst))
