4e788e947d371351e6ddb75631986e660d142976
mattfwilson <mattfwilson@gmail.com>
2022-04-06 13:23:35 -0400

    Playing around with nested inherited classes

--- inheritance.py
+++ inheritance.py
@@ -11,34 +11,25 @@ class Weapon():
 class Sword(Weapon):
     def __init__(self, dmg_type):
         self.dmg_type = dmg_type
-
-    def __repr__(self):
-        return self.dmg_type
         
     def say_type(self):
         print("Weapon damage type is {}.".format(self.dmg_type))
     
-    def swordSound(self):
+    def sword_sound(self):
         print("Slaaaash!")
+
+class IceSword(Sword):
+    def __init__(self, element):
+        self.element = element
+    
+    def say_element(self):
+        print("Weapon element is {}.".format(self.element))
 
 weaponList =[]
 
-weapon1 = Sword("Slashing")
-weaponList.append(weapon1)
+iced_sword = IceSword("Ice ice baby")
+iced_sword.sword_sound()
+iced_sword.say_element()
 
-weapon2 = Sword("Piercing")
-weaponList.append(weapon2)
-
-weapon3 = Sword("Clapping")
-weaponList.append(weapon3)
-
-for i in weaponList:
-    print(i)
-
-weapon2.say_type()
-weapon2.make_sound()
-
-weaponList[2].say_type() 
-weaponList[2].make_sound()
-
+print(f"Your weapon sounds like a {iced_sword.sword_sound()}")
 
