df4ee3a1e708289271e91d69fab5c66d5ca7deb2
mattfwilson <mattfwilson@gmail.com>
2022-04-08 15:21:38 -0400

    Added weighted rolling of stone types

--- gem_rush.py
+++ gem_rush.py
@@ -1,17 +1,19 @@
 import random
 
-stone_types = ["Granite", "Michah", "Quartz", "Limestone", "Diamond"]
-qualities = [1, 100]
+stone_types = ['Granite', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond']
 
-class Stone():
-    def __init__(self, type, quality):
+class Stone:
+    def __init__(self, type):
         self.rock_type = type
-        self.rarity = quality
 
-    def roll_type(self, type):
-        type_roll = random.choice(type)
-        return type_roll
+    def __str__(self, type):
+        print(type)
 
-mined_stone = Stone(stone_types, qualities)
-print(mined_stone)
+    def roll_type(self):
+        weighted_roll = random.choices(stone_types, weights=[10, 7, 5, 3, 2, 1])
+        return weighted_roll
+
+mined_stone = Stone(stone_types)
+print(mined_stone.roll_type()[0])
+
 
