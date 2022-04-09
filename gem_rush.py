3282f84e4e84c08b5b8aa40c48500e6a9c55a715
mattfwilson <mattfwilson@gmail.com>
2022-04-09 03:04:11 -0400

    Playing around with default parameters

--- gem_rush.py
+++ gem_rush.py
@@ -3,17 +3,15 @@ import random
 stone_types = ['Granite', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond']
 
 class Stone:
-    def __init__(self, type):
+    def __init__(self, type="Default"):
         self.rock_type = type
-
-    def __str__(self, type):
-        print(type)
 
     def roll_type(self):
         weighted_roll = random.choices(stone_types, weights=[10, 7, 5, 3, 2, 1])
         return weighted_roll
 
-mined_stone = Stone(stone_types)
-print(mined_stone.roll_type()[0])
+mined_stone = Stone()
+print(mined_stone.__init__("test"))
+print(mined_stone.roll_type())
 
 
