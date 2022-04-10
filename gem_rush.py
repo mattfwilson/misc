b185a8b24cd0eced4d389829a4aa8eff0e54d033
mattfwilson <mattfwilson@gmail.com>
2022-04-10 02:49:36 -0400

    Add default name parameter

--- gem_rush.py
+++ gem_rush.py
@@ -3,9 +3,10 @@ import random
 stone_types = ['Granite', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Moonstone']
 
 class Stone:
-    def __init__(self, type="Default"):
+    def __init__(self, type="Default", name="Name"):
         self.rock_type = type
-        print(type)
+        self.name = type
+        print(type, name)
 
     def roll_type(self):
         weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
