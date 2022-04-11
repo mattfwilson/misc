86623a6711e3bef615ff1c7fc421f1ed84f11561
mattfwilson <mattfwilson@gmail.com>
2022-04-11 00:30:35 -0400

    Rearranged parameters/arguments and formatted output roll

--- gem_rush.py
+++ gem_rush.py
@@ -3,15 +3,15 @@ import random
 stone_types = ['Granite', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Moonstone']
 
 class Stone:
-    def __init__(self, type="Default", name="Name"):
-        self.rock_type = type
+    def __init__(self, name="Default Name", type="Default Type"):
         self.name = type
-        print(type, name)
+        self.rock_type = type
+        print(f"{name}, {type}")
 
     def roll_type(self):
         weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
         return weighted_roll
 
-mined_stone1 = Stone("Not default")
+mined_stone1 = Stone("Matt Wilson", "Gold")
 mined_stone2 = Stone()
-print(mined_stone1.roll_type())
+print(f"You got a {mined_stone1.roll_type()[0]}!")
