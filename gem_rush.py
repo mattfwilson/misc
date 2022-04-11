9e5d448d0c6b89695091adb67032646c76c3239e
mattfwilson <mattfwilson@gmail.com>
2022-04-11 16:14:42 -0400

    Created conditional statements based on the quality of the weighted roll

--- gem_rush.py
+++ gem_rush.py
@@ -4,13 +4,19 @@ stone_types = ['Granite', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Mo
 
 class Stone:
     new_id = 1
-    def __init__(self, name="Default Name", type="Default Type"):
+    rarity = 0
+    def __init__(self, name="Default Name", type="Default Type", rarity):
         self.id = Stone.new_id
         Stone.new_id += 1
         self.name = type
         self.rock_type = type
+        self.rarity = rarity
         print(f"{name}, {type}, {self.id}")
 
+    def roll_type(self):
+        weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
+        return weighted_roll
+    
     def roll_type(self):
         weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
         return weighted_roll
---
+++ gem_rush2.py
@@ -0,0 +1,24 @@
+import random
+
+class Stone:
+    create_id = 1
+    def __init__(self):
+        stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
+        self.id = Stone.create_id
+        Stone.create_id += 1
+
+        weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
+        
+        if weighted_roll[0] == stone_types[0]:
+            print(f"Roll {self.id}: Bad luck... {weighted_roll}")
+        elif weighted_roll[0] == stone_types[-1]:
+            print(f"Roll {self.id}: You got something amazing! {weighted_roll}")
+        else:
+            print(f"Roll {self.id}: You got something good! {weighted_roll}")
+        
+
+mined_stone1 = Stone()
+mined_stone2 = Stone()
+mined_stone3 = Stone()
+mined_stone4 = Stone()
+mined_stone5 = Stone()
