83498daf724527089170064655005881fa0a52cf
mattfwilson <mattfwilson@gmail.com>
2022-04-12 17:26:50 -0400

    Created new stone_inv list to append all class rolls to

--- gem_rush2.py
+++ gem_rush2.py
@@ -1,13 +1,16 @@
 import random
 
+stone_inv = []
+
 class Stone:
     create_id = 1
     def __init__(self):
-        stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
-        bad_luck = stone_types[0]
-
         self.id = Stone.create_id
         Stone.create_id += 1
+
+    def roll(self):
+        stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
+        bad_luck = stone_types[0]
 
         weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
         roll_str = weighted_roll[0]
@@ -18,9 +21,16 @@ class Stone:
             print(f"Roll {self.id}: You got something amazing! {weighted_roll}")
         else:
             print(f"Roll {self.id}: You got something good! {weighted_roll}")
+        return roll_str
+
     
-mined_stone1 = Stone()
-mined_stone2 = Stone()
-mined_stone3 = Stone()
-mined_stone4 = Stone()
-mined_stone5 = Stone()
+mined_stone1 = Stone().roll()
+stone_inv.append(mined_stone1)
+mined_stone2 = Stone().roll()
+stone_inv.append(mined_stone2)
+mined_stone3 = Stone().roll()
+stone_inv.append(mined_stone3)
+
+print(stone_inv)
+
+
