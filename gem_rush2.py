f3b2a0f809b2420cb89961d989dd2371d08ddc70
mattfwilson <mattfwilson@gmail.com>
2022-04-13 02:26:23 -0400

    Simplified the conditional if checking if the stone type is a pebble

--- gem_rush2.py
+++ gem_rush2.py
@@ -11,17 +11,15 @@ class Stone:
     def roll(self):
         stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
         bad_luck = stone_types[0]
-
         weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 1])
-        roll_str = weighted_roll[0]
 
-        if roll_str == bad_luck:
+        if weighted_roll[0] == bad_luck:
             print(f"Roll {self.id}: Bad luck... {weighted_roll}")
         elif weighted_roll[0] == stone_types[-1]:
             print(f"Roll {self.id}: You got something amazing! {weighted_roll}")
         else:
             print(f"Roll {self.id}: You got something good! {weighted_roll}")
-        return roll_str
+        return weighted_roll
 
     
 mined_stone1 = Stone().roll()
