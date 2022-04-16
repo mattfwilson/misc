bccaaab3cc9f2ad719e939cbc37beaca9be070b2
mattfwilson <mattfwilson@gmail.com>
2022-04-16 04:26:03 -0400

    Added for loop to conditional check on the stone type

--- gem_rush2.py
+++ gem_rush2.py
@@ -10,17 +10,20 @@ class Stone:
 
     def roll(self):
         stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
-        weighted_roll = random.choices(stone_types, weights=[10, 8, 6, 4, 3, 2, 10])
+        weighted_roll = random.choices(stone_types, weights=[8, 8, 6, 4, 3, 2, 2])
         print(f"Roll {self.id}: {weighted_roll}")
         return weighted_roll
 
 def check_quality(roll_lst):
-    if "Moonstone" in roll_lst:
-        print(f"You found a Moonstone. V rare!")
-    elif "Diamond" in roll_lst:
-        print(f"You found a Diamond. Stellar!")
-    else:
-        print(f"Ehh...")
+    for i in roll_lst:
+        if i == "Moonstone":
+            print(f"You found a {i}. V rare!")
+        elif i == "Diamond":
+            print(f"You found a {i}. Dope AF!")
+        elif i == "Platinum" or i == "Gold" or i == "Silver":
+            print(f"We'll take a {i}!")
+        else:
+            print(f"{i}... Ehh, not great.")
 
 mined_stone1 = Stone().roll()
 inventory.append(mined_stone1[0])
