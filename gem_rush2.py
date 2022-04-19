7df84d18988d59ef6c2bc441e76d3e4e305ba7d1
mattfwilson <mattfwilson@gmail.com>
2022-04-19 02:24:02 -0400

    Still trying to figure out why the list index is out of range

--- gem_rush2.py
+++ gem_rush2.py
@@ -9,21 +9,21 @@ class Stone:
         Stone.create_id += 1
 
     def roll(self):
-        stone_types = ["Pebble", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Moonstone"]
-        weighted_roll = random.choices(stone_types, weights=[8, 8, 6, 4, 3, 2, 2])
-        print(f"Roll {self.id}: {weighted_roll}")
+        stone_types = ['Pebble', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Moonstone']
+        weighted_roll = random.choices(stone_types, weights=[7, 6, 5, 4, 3, 2, 1])
+        print(f'Roll {self.id}: {weighted_roll}')
         return weighted_roll
 
 def check_quality(roll_lst):
     for i in roll_lst:
         if i == roll_lst[5] or i == roll_lst[6]:
-            print(f"You found a {i}. V rare!")
+            print(f'You found a {i}. V rare!')
         elif i == roll_lst[3] or i == roll_lst[4]:
-            print(f"You found a {i}. Dope AF!")
-        elif i == roll_lst[1] or i == roll_lst == roll_lst[2]:
-            print(f"We'll take a {i}!")
-        elif i == roll_lst[0]:
-            print(f"{i}... Ehh, not great.")
+            print(f'You found a {i}. Dope AF!')
+        elif i == roll_lst[1] or i == roll_lst[2]:
+            print(f'We take {i}s!')
+        else:
+            print(f'{i}... Ehh, not great.')
 
 mined_stone1 = Stone().roll()
 inventory.append(mined_stone1[0])
@@ -36,7 +36,6 @@ inventory.append(mined_stone3[0])
 
 print(inventory)
 check = check_quality(inventory)
-
 
 
 
