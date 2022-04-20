d90378ecb713d46b0688607fde47ece659f720da
mattfwilson <mattfwilson@gmail.com>
2022-04-20 16:14:30 -0400

    Added simple search functionality

--- lst_add.py
+++ lst_add.py
@@ -1,8 +1,13 @@
 lst = ['Apple', 'Pear', 'Orange']
+query = 'Apple'
 
 if len(lst) < 10:
     while len(lst) < 10:
         lst.append('New Fruit')
         print(lst)
-else:
     print('List is full!')
+    for i in lst:
+        if i == query:
+            print(f'Found {query} in your list')
+        else:
+            print(f'No results for {query}')
