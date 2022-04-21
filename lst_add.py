7407bbe0961bc83966f0d72b7efabfda48dd538f
mattfwilson <mattfwilson@gmail.com>
2022-04-21 02:04:34 -0400

    Added check for if 'Kiwi' was in the list and else for adding 'KIWI' if not

--- lst_add.py
+++ lst_add.py
@@ -1,15 +1,19 @@
-lst = ['Apple', 'Pear', 'Orange']
+lst = ['Apple', 'Pear', 'Orange', 'Kiwi']
 query = ''
 
 query = input('What do you want to search for? ')
 
-if len(lst) < 10:
-    while len(lst) < 10:
-        lst.append('Apple')
-        print(lst)
-    print('List is full!')
-    for i in lst:
-        if i == query:
-            print(f'{i} matches your query: {query}')
-        else:
-            print(f'{i} does not match your query: {query}')
+if len(lst) < 20:
+    if "Kiwi" not in lst:
+        while len(lst) < 10:
+            lst.append('Kiwi')
+        print('List is full!')
+        for i in lst:
+            if i == query:
+                print(f'{i} matches your query: {query}')
+            else:
+                print(f'{i} does not match your query: {query}')
+    else:
+        while len(lst) < 10:
+            lst.append('KIWI')
+            print(lst)
