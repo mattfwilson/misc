6a9b1f0b3e39f04a8146be177f364dcd3ae8a360
mattfwilson <mattfwilson@gmail.com>
2022-04-21 03:32:38 -0400

    Trying to get user input for bill, tip, and tax to work

--- high_lvl_func2.py
+++ high_lvl_func2.py
@@ -1,10 +1,21 @@
+tip = 0
+tax = 0
+bill = 0
+
+bill = input('How much is your bill? ')
+tip = input('How much do you want to tip? ')
+tax = input('How much is tax? ')
+
+print(type(tip))
+print(type(tax))
+
 def total_bill(total, tip_n_tax):
     return total + tip_n_tax(total)
 
 def tip_tax(bill):
-    tip = bill * .20
-    tax = bill * .06
-    total = tip + tax
+    tipped = bill * tip
+    taxed = bill * tax
+    total = tipped + taxed
     return total
 
-print(total_bill(100, tip_tax))
+result = total_bill(bill, tip_tax)
