28e4dcb78e664aad0198856434e55ee5cf5da803
mattfwilson <mattfwilson@gmail.com>
2022-04-26 03:52:51 -0400

    Consolidated everything into one function and fixed state tax not calculating

--- high_lvl_func2.py
+++ high_lvl_func2.py
@@ -1,24 +1,19 @@
-tip = .20
-tax = .06
+tip_percent = .20
+fed_tax = .06
 state_tax = .03
-bill = 0
+total_bill = 0
 
-bill = input('How much is your bill? ')
-bill = int(bill)
-
-def total_bill(total):
-    return ("The total amount owed is $" + "{:.2f}".format(total))
+total_bill = input('How much is your bill? ')
+total_bill = int(total_bill)
 
 def tip_tax(bill, tax, state, tip):
     taxed = bill * tax
-    stated = taxed * state
-    total = stated * tip
-    return total
+    stated = bill * state
+    tipped = bill * tip
+    total = bill + taxed + stated + tipped
+    print('The total amount owed is $' + '{:.2f}'.format(total))
+    print('Fed tax: $' + '{:.2f}'.format(taxed))
+    print('State tax: $' + '{:.2f}'.format(stated))
+    print('Tip: $' + '{:.2f}'.format(tipped))
 
-def state_tax(bill):
-    state = bill * .04
-    state_total = state + bill
-    return state_total
-
-result = total_bill(bill)
-print(result)
+result = tip_tax(total_bill, fed_tax, state_tax, tip_percent)
