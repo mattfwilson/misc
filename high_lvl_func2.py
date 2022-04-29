6ac3ff2d1334c23c156bdf7a5c946c5d64251128
mattfwilson <mattfwilson@gmail.com>
2022-04-29 01:45:34 -0400

    Added user input for federal and state tax amounts in percentages

--- high_lvl_func2.py
+++ high_lvl_func2.py
@@ -1,12 +1,16 @@
-tip_percent = .20
-fed_tax = .06
-state_tax = .03
+tip_percent = 0
+fed_tax = 0
+state_tax = 0
 total_bill = 0
 
 total_bill = input('How much is your bill? ')
 total_bill = int(total_bill)
 tip_percent = input('How much tip in percent? ')
 tip_percent = float(tip_percent)
+fed_percent = input('How much is fed tax in percent? ')
+fed_percent = float(fed_percent)
+state_percent = input('How much is state tax in percent? ')
+state_percent = float(state_percent)
 
 def tip_tax(bill, tax, state, tip):
     taxed = bill * tax
@@ -18,4 +22,4 @@ def tip_tax(bill, tax, state, tip):
     print('Tip: $' + '{:.2f}'.format(tipped))
     return print('The total amount owed is $' + '{:.2f}'.format(total))
 
-result = tip_tax(total_bill, fed_tax, state_tax, tip_percent)
+result = tip_tax(total_bill, fed_percent, state_percent, tip_percent)
