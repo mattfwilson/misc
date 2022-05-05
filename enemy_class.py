4fe545a6fea3ff7235fd8e8d16d4ae8601f0061d
mattfwilson <mattfwilson@gmail.com>
2022-05-05 02:59:40 -0400

    Removed method returning HP/MP and added overriding method for new Flying class to output the movement type for the child class

--- enemy_class.py
+++ enemy_class.py
@@ -1,19 +1,11 @@
-class Fire:
-    FIRE_DMG = 5
-
-    def __init__(self):
-        self.fire_dmg = Fire.FIRE_DMG
-    
-    def bonusDamage(self):
-        print("Adding {} bonus points of fire damage!".format(self.fire_dmg))
-
-class Enemy(Fire):
+class Enemy():
     ID = 1
     HP = 50
     MP = 10
     ATT = 12
     DEF = 7
     END = 22.6
+    MOVEMENT = 'walking'
 
     def __init__(self):
         self.enemy_id = Enemy.ID
@@ -22,25 +14,29 @@ class Enemy(Fire):
         self.enemy_att = Enemy.ATT
         self.enemy_def = Enemy.DEF
         self.enemy_end = Enemy.END
+        self.enemy_movement = Enemy.MOVEMENT
         Enemy.ID += 1
 
     def say_ID(self):
-        print("Enemy ID: {}".format(self.enemy_id))
+        print("Enemy ID: {}".format(Enemy.ID))
+    
+    def movement(self):
+        print("Enemy {} is {}".format(Enemy.ID, Enemy.MOVEMENT))
 
-    def say_HP_MP(self):
-        print("Enemy HP: {}, MP: {}, END: {}".format(self.enemy_hp, self.enemy_mp, self.enemy_end))
+
+class Flying(Enemy):
+    MOVEMENT = 'fLying'
+
+    def __init__(self):
+        self.enemy_movement = Flying.MOVEMENT
+
+    def movement(self):
+        print("Enemy {} is {}".format(Enemy.ID, Flying.MOVEMENT))
     
 e1 = Enemy()
-e1.say_ID()
+e1.movement()
 
-e2 = Enemy()
-e2.say_HP_MP()
-
-e3 = Enemy()
-e3.say_HP_MP()
-
-e4 = Enemy()
-e4.say_HP_MP()
-e4.bonusDamage()
+e2 = Flying()
+e2.movement()
 
 
