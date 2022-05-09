3b82f946e8d88fe1d46e690b966a64f135029d36
mattfwilson <mattfwilson@gmail.com>
2022-05-09 16:06:10 -0400

    Changed movement method to become method type

--- enemy_class.py
+++ enemy_class.py
@@ -5,59 +5,59 @@ class Enemy():
     ATT = 12
     DEF = 7
     END = 22.6
-    MOVEMENT = 'walking'
+    TYPE = 'Normal'
 
     def __init__(self):
         self.enemy_id = Enemy.ID
+        self.enemy_type = Enemy.TYPE
         self.enemy_hp = Enemy.HP
         self.enemy_mp = Enemy.MP
         self.enemy_att = Enemy.ATT
         self.enemy_def = Enemy.DEF
         self.enemy_end = Enemy.END
-        self.enemy_movement = Enemy.MOVEMENT
         Enemy.ID += 1
 
     def say_ID(self):
         print("Enemy ID: {}".format(Enemy.ID))
     
-    def movement(self):
-        print("Enemy {} is {}".format(Enemy.ID, Enemy.MOVEMENT))
+    def say_type(self):
+        print("Enemy {} is {}".format(Enemy.ID, Enemy.TYPE))
 
-
-class Flying(Enemy):
-    MOVEMENT = 'fLying'
+class Fire(Enemy):
+    TYPE = 'Fire'
 
     def __init__(self):
         # super().__init__(self)
         self.enemy_id = Enemy.ID
-        self.enemy_movement = Flying.MOVEMENT
+        self.enemy_movement = Fire.TYPE
 
     def say_ID(self):
         print("Enemy ID: {}".format(Enemy.ID))
 
-    def movement(self):
-        print("Enemy {} is {}".format(Enemy.ID, Flying.MOVEMENT))
+    def say_type(self):
+        print("Enemy {} is {}".format(Enemy.ID, Fire.TYPE))
 
 class Water(Enemy):
-    MOVEMENT = 'swimming'
+    TYPE = 'Water'
 
     def __init__(self):
         self.enemy_id = Enemy.ID
-        self.enemy_movement = Water.MOVEMENT
+        self.enemy_type = Water.TYPE
 
     def say_ID(self):
         print("Enemy ID: {}".format(Enemy.ID))
 
-    def movement(self):
-        print("Enemy {} is {}".format(Enemy.ID, self.enemy_movement))
+    def say_type(self):
+        print("Enemy {} is {}".format(Enemy.ID, self.enemy_type))
     
 e1 = Enemy()
-e2 = Flying()
+e2 = Fire()
 e3 = Water()
 
 enemies = [e1, e2, e3]
+
 for i in enemies:
     i.say_ID()
-    i.movement()
+    i.say_type()
 
 
