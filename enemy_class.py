d26e020dfc83f76f6e1f941ef0e947f424546d37
mattfwilson <mattfwilson@gmail.com>
2022-05-10 01:57:04 -0400

    Simplifying code to try and understand why inheritance of say_type() isn't working

--- enemy_class.py
+++ enemy_class.py
@@ -1,63 +1,29 @@
 class Enemy():
-    ID = 0
-    HP = 50
-    MP = 10
-    ATT = 12
-    DEF = 7
-    END = 22.6
-    TYPE = 'Normal'
 
     def __init__(self):
-        self.enemy_id = Enemy.ID
-        self.enemy_type = Enemy.TYPE
-        self.enemy_hp = Enemy.HP
-        self.enemy_mp = Enemy.MP
-        self.enemy_att = Enemy.ATT
-        self.enemy_def = Enemy.DEF
-        self.enemy_end = Enemy.END
-        Enemy.ID += 1
-
-    def say_ID(self):
-        print("Enemy ID: {}".format(Enemy.ID))
+        self.enemy_id = 0
+        self.enemy_type = 'Normal'
+        self.enemy_hp = 50
+        self.enemy_id += 1
     
     def say_type(self):
-        print("Enemy {} is {}".format(Enemy.ID, Enemy.TYPE))
+        print("Enemy {} is {} type".format(self.enemy_id, self.enemy_type))
 
 class Fire(Enemy):
-    TYPE = 'Fire'
 
     def __init__(self):
-        # super().__init__(self)
-        self.enemy_id = Enemy.ID
-        self.enemy_movement = Fire.TYPE
-
-    def say_ID(self):
-        print("Enemy ID: {}".format(Enemy.ID))
-
-    def say_type(self):
-        print("Enemy {} is {}".format(Enemy.ID, Fire.TYPE))
+        self.enemy_type = 'Fire'
 
 class Water(Enemy):
-    TYPE = 'Water'
 
     def __init__(self):
-        self.enemy_id = Enemy.ID
-        self.enemy_type = Water.TYPE
-
-    def say_ID(self):
-        print("Enemy ID: {}".format(Enemy.ID))
-
-    def say_type(self):
-        print("Enemy {} is {}".format(Enemy.ID, self.enemy_type))
+        self.enemy_type = 'Water'
     
 e1 = Enemy()
 e2 = Fire()
 e3 = Water()
 
-enemies = [e1, e2, e3]
-
-for i in enemies:
-    i.say_ID()
-    i.say_type()
-
+print(e1.say_type())
+print(e2.say_type())
+print(e3.say_type())
 
