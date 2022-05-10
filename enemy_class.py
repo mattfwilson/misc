67ee46c09d9d0a29b4f93d3dad17f3297bdf7d03
mattfwilson <mattfwilson@gmail.com>
2022-05-10 11:20:27 -0400

    Experiment with color contrast formula + fixing inheritance with enemy_class.py

--- enemy_class.py
+++ enemy_class.py
@@ -1,28 +1,24 @@
-class Enemy():
+class Enemy:
     id = 1
 
-    def __init__(self):
-        self.enemy_id = Enemy.id
-        self.enemy_type = 'Normal'
-        self.enemy_hp = 100
-        Enemy.id += 1
+    def __init__(self, type, hp):
+        self.enemy_type = type
+        self.enemy_hp = hp
+        self.id += 1
 
     def say_type(self):
-        print("Enemy {} is type {} and has {} HP.".format(self.enemy_id, self.enemy_type, self.enemy_hp))
+        print("Enemy {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
 
 class Fire(Enemy):
-    def __init__(self):
-        self.enemy_type = 'Fire'
+    def say_type(self):
+        print("Enemy {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
 
-class Water(Enemy):
-    def __init__(self):
-        self.enemy_type = 'Water'
     
-e1 = Enemy()
-e2 = Fire()
-e3 = Water()
+e1 = Enemy("Flying", 50)
+e2 = Fire("Noob", 1)
+
 
 print(e1.say_type())
 print(e2.say_type())
-print(e3.say_type())
+# print(help(e1))
 
