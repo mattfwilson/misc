930f8d0813a1286d0fb21e5b4579813209bb3330
mattfwilson <mattfwilson@gmail.com>
2022-05-15 23:59:32 -0400

    Further debugging Boss class

--- enemy_class.py
+++ enemy_class.py
@@ -9,15 +9,15 @@ class Enemy:
     def say_type(self):
         print("Enemy {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
 
-# class Boss(Enemy):
+class Boss(Enemy):
 
-#     def __init__(self, enemy_type, enemy_hp, boss_hp):
-#         super().__init__(enemy_type, enemy_hp, boss_hp)
-#         self.boss_hp = boss_hp
-#         boss_health = enemy_hp * self.boss_hp
+    def __init__(self, enemy_type, enemy_hp, boss_hp):
+        super().__init__(enemy_type, enemy_hp)
+        self.boss_hp = boss_hp
+        boss_health = enemy_hp + self.boss_hp
 
-#     def say_type(self):
-#         print("Enemy {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
+    def say_type(self):
+        print("Boss {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp * 2))
 
 # class Megaboss(Enemy):
 #     def __init__(self, enemy_type, enemy_hp, megaboss_hp, underlings=None):
@@ -43,7 +43,8 @@ class Enemy:
 e1 = Enemy('Normal', 50)
 e1.say_type()
 
-# e2 = Boss('Fire', 50, 4)
+e2 = Boss('Boss', 50, 4)
+e2.say_type()
 # e2 = Megaboss('Ice', 50, 100)
 
 print(e1)
