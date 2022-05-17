332e37aab91a918852422e429fbabc33b4cbc9bf
mattfwilson <mattfwilson@gmail.com>
2022-05-17 11:25:17 -0400

    Deleted while loop method of creating bridge

--- glass_bridge.py
+++ glass_bridge.py
@@ -1,19 +1,14 @@
 import random
 
 bridge = []
-bridge2 = []
-tiles = 0
-tiles2 = 16
+tiles = 16
 options = ['left', 'right']
 
-while tiles < 16:
-    tile = random.randint(0, 1)
-    bridge.append(tile)
-    tiles += 1
-    print(bridge)
-
-
-for i in range(tiles2):
+for i in range(tiles):
     tile = random.choice(options)
-    bridge2.append(tile)
-    print(bridge2)
+    bridge.append(tile)
+
+print(bridge)
+
+for i in bridge:
+    print(i)
