4b9288a50a83a4c841344d1d0b6a4091c4458b31
mattfwilson <mattfwilson@gmail.com>
2022-05-17 15:11:24 -0400

    Started the conditional checks for the user guess and the actual bridge

--- glass_bridge.py
+++ glass_bridge.py
@@ -1,14 +1,26 @@
 import random
 
-bridge = []
-tiles = 16
-options = ['left', 'right']
+BRIDGE = []
+USER_BRIDGE = []
+GUESS_NUM = 0
+TILES = 16
+OPTIONS = ['left', 'right']
 
-for i in range(tiles):
-    tile = random.choice(options)
-    bridge.append(tile)
+for i in range(TILES):
+    tile = random.choice(OPTIONS)
+    BRIDGE.append(tile)
 
-print(bridge)
+while GUESS_NUM < 17:
+    guess = input(f'What is your guess? ')
+    USER_BRIDGE.append(guess)
+    if USER_BRIDGE[GUESS_NUM] == BRIDGE[GUESS_NUM]:
+        print(f'{guess} tile is safe!')
+        print(f'User bridge :{USER_BRIDGE}')
+        print(f'Real bridge: {BRIDGE}')
+        print(USER_BRIDGE[GUESS_NUM])
+    elif USER_BRIDGE[GUESS_NUM] != BRIDGE[GUESS_NUM]:
+        print(f'Uh oh {guess} tile broke... you ded!')
+        print(f'User bridge :{USER_BRIDGE}')
+        print(f'Real bridge: {BRIDGE}')
+        print(USER_BRIDGE[GUESS_NUM])
 
-for i in bridge:
-    print(i)
