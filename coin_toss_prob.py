f0de126628bcc3a821014b204b1d4cd824f03076
mattfwilson <mattfwilson@gmail.com>
2022-05-18 00:39:02 -0400

    Added formatted probability percentages

--- coin_toss_prob.py
+++ coin_toss_prob.py
@@ -4,19 +4,29 @@ FLIPS = 1000
 OUTCOMES = ['HH', 'TT', 'HT', 'TH']
 RESULTS = []
 
-while FLIPS > 0:
+for flip in range(FLIPS):
     toss = random.choice(OUTCOMES)
     RESULTS.append(toss)
-    FLIPS -= 1
 
 print(RESULTS)
 
 heads_heads = RESULTS.count('HH')
-heads_tails = RESULTS.count('HT')
-tails_heads = RESULTS.count('TH')
-tails_tails = RESULTS.count('TT')
+HH_percent = heads_heads / FLIPS
+HH_formatted = "{:.0%}".format(HH_percent)
 
-print(f'HH: {heads_heads}')
-print(f'HT: {heads_tails}')
-print(f'TH: {tails_heads}')
-print(f'TT: {tails_tails}')
+heads_tails = RESULTS.count('HT')
+HT_percent = heads_tails / FLIPS
+HT_formatted = "{:.0%}".format(HT_percent)
+
+tails_heads = RESULTS.count('TH')
+TH_percent = tails_heads / FLIPS
+TH_formatted = "{:.0%}".format(TH_percent)
+
+tails_tails = RESULTS.count('TT')
+TT_percent = tails_tails / FLIPS
+TT_formatted = "{:.0%}".format(TT_percent)
+
+print(f'Heads/Heads: {heads_heads} outcomes, representing probability of {HH_formatted}')
+print(f'Heads/Tails: {heads_tails} outcomes, representing probability of {HT_formatted}')
+print(f'Tails/Heads: {tails_heads} outcomes, representing probability of {TH_formatted}')
+print(f'Tails/Tails: {tails_tails} outcomes, representing probability of {TT_formatted}')
