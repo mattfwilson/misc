0d1081bcf7718eb3db11574b49c9ee35c7d8f69c
mattfwilson <mattfwilson@gmail.com>
2022-05-29 22:26:58 -0400

    Changed some var names to be clearer

--- glass_bridge.py
+++ glass_bridge.py
@@ -1,26 +1,26 @@
 import random
 
-BRIDGE = []
+ACTUAL_BRIDGE = []
 USER_BRIDGE = []
 GUESS_NUM = 0
 TILES = 16
 OPTIONS = ['left', 'right']
 
-for i in range(TILES):
-    tile = random.choice(OPTIONS)
-    BRIDGE.append(tile)
+for tile in range(TILES):
+    actual_tiles = random.choice(OPTIONS)
+    ACTUAL_BRIDGE.append(actual_tiles)
 
 while GUESS_NUM < 17:
     guess = input(f'What is your guess? ')
     USER_BRIDGE.append(guess)
-    if USER_BRIDGE[GUESS_NUM] == BRIDGE[GUESS_NUM]:
+    if USER_BRIDGE[GUESS_NUM] == ACTUAL_BRIDGE[GUESS_NUM]:
         print(f'{guess} tile is safe!')
         print(f'User bridge :{USER_BRIDGE}')
-        print(f'Real bridge: {BRIDGE}')
+        print(f'Real bridge: {ACTUAL_BRIDGE}')
         print(USER_BRIDGE[GUESS_NUM])
-    elif USER_BRIDGE[GUESS_NUM] != BRIDGE[GUESS_NUM]:
+    elif USER_BRIDGE[GUESS_NUM] != ACTUAL_BRIDGE[GUESS_NUM]:
         print(f'Uh oh {guess} tile broke... you ded!')
         print(f'User bridge :{USER_BRIDGE}')
-        print(f'Real bridge: {BRIDGE}')
+        print(f'Real bridge: {ACTUAL_BRIDGE}')
         print(USER_BRIDGE[GUESS_NUM])
 
