cf87a20ef180977f17434a08f521fff9ca49ecb0
mattfwilson <mattfwilson@gmail.com>
2022-06-13 00:44:37 -0400

    Sum of trip expenses with Janelle

---
+++ sum_janelle.py
@@ -0,0 +1,11 @@
+tripExpenses = {
+    'car rental': 124.60,
+    'hertz tolls': 19.00,
+    'lobster cove': 65.62,
+    'oceanside shack': 42.08,
+    'bagel basket': 23.27,
+    'car gas': 18.69
+    }
+
+total = sum(tripExpenses.values()) / 2
+print(total)
