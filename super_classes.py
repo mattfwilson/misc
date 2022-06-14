b77517b132403f8d308573e5a6c4e6fff60a4c68
mattfwilson <mattfwilson@gmail.com>
2022-06-14 00:12:52 -0400

    Added color and took out volume x2 method

--- super_classes.py
+++ super_classes.py
@@ -1,32 +1,32 @@
 cubeVolume = 2
 
 class Rectangle:
-    def __init__(self, length, width):
+    def __init__(self, length, width, color):
         self.length = length
         self.width = width
+        self.color = color
 
 class Square(Rectangle):
-    def __init__(self, length, width):
-        super().__init__(length, width)
+    def __init__(self, length, width, color):
+        super().__init__(length, width, color)
 
     def area(self):
         return self.length * self.width
 
 class Cube(Rectangle):
-    def __init__(self, length, width, height):
-        super().__init__(length, width)
+    def __init__(self, length, width, height, color):
+        super().__init__(length, width, color)
         self.height = height
     
     def volume(self):
         cubeVolume = self.length * self.width * self.height
         return cubeVolume
     
-    def volumeX2(self, volume):
-        volDoubled = volume * 2
-        return volDoubled
+    def printColor(self, color):
+        return color
 
-square1 = Square(8, 5)
+square1 = Square(8, 5, 'Blue')
 print(square1.area())
 
-cube1 = Cube(2, 4, 6)
-print(cube1.volumeX2(cubeVolume))
+cube1 = Cube(2, 4, 6, 'Green')
+print(cube1.printColor('Red'))
