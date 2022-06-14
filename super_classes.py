4933196655612b85dc019854c0d78bc71fce67fc
mattfwilson <mattfwilson@gmail.com>
2022-06-14 01:18:13 -0400

    Created measurements method and formatted the color and volume output strings

--- super_classes.py
+++ super_classes.py
@@ -6,27 +6,21 @@ class Rectangle:
         self.width = width
         self.color = color
 
-class Square(Rectangle):
-    def __init__(self, length, width, color):
-        super().__init__(length, width, color)
-
-    def area(self):
-        return self.length * self.width
-
 class Cube(Rectangle):
-    def __init__(self, length, width, height, color):
+    def __init__(self, length, width, height, color='bland white'):
         super().__init__(length, width, color)
         self.height = height
     
+    def measurements(self):
+        return self.length, self.width, self.height
+
     def volume(self):
         cubeVolume = self.length * self.width * self.height
         return cubeVolume
     
-    def printColor(self, color):
-        return color
+    def printColor(self):
+        return self.color
 
-square1 = Square(8, 5, 'Blue')
-print(square1.area())
-
-cube1 = Cube(2, 4, 6, 'Green')
-print(cube1.printColor('Red'))
+cube1 = Cube(2, 4, 6)
+print(f'Your {cube1.printColor()} cube has a length of {cube1.measurements()[0]}, width of {cube1.measurements()[1]}, and a height of {cube1.measurements()[2]}.')
+print(f'The volume of your cube is {cube1.volume()}.')
