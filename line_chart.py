eb8a495c87ae45101880436184534dcf5462e315
mattfwilson <mattfwilson@gmail.com>
2022-06-15 04:19:24 -0400

    Fixed error with chart not showing (didn't link .pyplot upon import)

--- line_chart.py
+++ line_chart.py
@@ -1,8 +1,10 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 avgTemps = [17, 14, 26, 41, 67, 77, 82, 86, 85, 69, 44, 32]
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 fig, ax = plt.subplots(figsize=(10, 6))
 
-ax.subplot(months, avgTemps)
+ax.plot(months, avgTemps)
+
+plt.show()
