94df20b56dea46922b73914bc4700a94e8f66302
mattfwilson <mattfwilson@gmail.com>
2022-06-15 01:45:46 -0400

    Added appending of objects to inventory list

--- super_classes.py
+++ super_classes.py
@@ -1,3 +1,5 @@
+inventory = []
+
 class Rectangle:
     def __init__(self, length, width, color):
         self.length = length
@@ -8,15 +10,17 @@ class Cube(Rectangle):
     def __init__(self, length, width, height, color='bland white'):
         super().__init__(length, width, color)
         self.height = height
+        inventory.append(self)
 
     def volume(self):
         cubeVolume = self.length * self.width * self.height
         return cubeVolume
-    
-    def printColor(self):
-        return self.color
 
 cube1 = Cube(2, 4, 6, 'sky blue')
+cube2 = Cube(50, 100, 777, 'crystal clear')
+cube3 = Cube(1, 1, 1, 'neon pink')
+
+print(inventory)
 
 if cube1.color == "sky blue":
     print(f'Your {cube1.color} cube has a length of {cube1.length}, width of {cube1.width}, and a height of {cube1.height}.')
