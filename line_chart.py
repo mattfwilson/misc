69f697853732c3dd8e56f9bfe9d8defa6bbab332
mattfwilson <mattfwilson@gmail.com>
2022-06-16 04:04:07 -0400

    Made the line chart produce random temperatures for each month

--- line_chart.py
+++ line_chart.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import random
 
-avgTemps = [17, 14, 26, 41, 67, 77, 82, 86, 85, 69, 44, 32]
+
+avgTemps = []
+# 17, 14, 26, 41, 67, 77, 82, 86, 85, 69, 44, 32
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+for i in range(len(months)):
+    temp = random.randint(0, 100)
+    avgTemps.append(temp)
+
 fig, ax = plt.subplots(figsize=(10, 6))
-
 ax.plot(months, avgTemps)
-
 plt.show()
