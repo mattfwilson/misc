3e402c740ea1f72dfc7a2e843c567a31a4afd925
mattfwilson <mattfwilson@gmail.com>
2022-06-17 04:23:00 -0400

    Trying to get ids to increment properly

--- super_classes.py
+++ super_classes.py
@@ -1,15 +1,18 @@
 inventory = []
 
 class Rectangle:
+
+    id = 0
+
     def __init__(self, length, width, color):
-        self.id = id
         self.length = length
         self.width = width
         self.color = color
+        self.id += 1
 
 class Cube(Rectangle):
 
-    def __init__(self, length, width, height, color='bland white'):
+    def __init__(self, length, width, height, color='bland white',):
         super().__init__(length, width, color)
         self.height = height
         inventory.append(self)
@@ -19,19 +22,19 @@ class Cube(Rectangle):
         return cubeVolume
     
     def __repr__(self):
-        return f'Cube height: {self.height}'
+        return f'Cube {self.id} height: {self.height}'
 
 cube1 = Cube(2, 4, 6, 'sky blue')
 cube2 = Cube(50, 100, 777, 'crystal clear')
-cube3 = Cube(1, 1, 1, 'neon pink')
+cube3 = Cube(-3, -2, -8, 'neon pink')
 
 print(inventory)
 
-if cube1.color == "sky blue":
+if cube1.color == "neon pink":
     print(f'Your {cube1.color} cube has a length of {cube1.length}, width of {cube1.width}, and a height of {cube1.height}.')
     print(f'The volume of your cube is {cube1.volume()}.')
 else:
-    print(f'Your cube has no specified color but has a length of {cube1.length}, width of {cube1.width}, and a height of {cube1.height}.')
-    print(f'The volume of your cube is {cube1.volume()}.')
+    print(f'Your cube has no specified color but has a length of {cube2.length}, width of {cube2.width}, and a height of {cube2.height}.')
+    print(f'The volume of your cube is {cube2.volume()}.')
 
 
