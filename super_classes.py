9e0ebb7c3cdb30a93e96d9cccd5cad1fc54efdcb
mattfwilson <mattfwilson@gmail.com>
2022-06-17 01:47:51 -0400

    Added repr

--- super_classes.py
+++ super_classes.py
@@ -2,11 +2,13 @@ inventory = []
 
 class Rectangle:
     def __init__(self, length, width, color):
+        self.id = id
         self.length = length
         self.width = width
         self.color = color
 
 class Cube(Rectangle):
+
     def __init__(self, length, width, height, color='bland white'):
         super().__init__(length, width, color)
         self.height = height
@@ -15,6 +17,9 @@ class Cube(Rectangle):
     def volume(self):
         cubeVolume = self.length * self.width * self.height
         return cubeVolume
+    
+    def __repr__(self):
+        return f'Cube height: {self.height}'
 
 cube1 = Cube(2, 4, 6, 'sky blue')
 cube2 = Cube(50, 100, 777, 'crystal clear')
