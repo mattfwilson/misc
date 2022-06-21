0f9b7e53d67f508c259d62ae30abe945ce7fcbc9
mattfwilson <mattfwilson@gmail.com>
2022-06-21 01:29:39 -0400

    Made new file without Megaboss class. Fixed increment of enemy id not starting at 1

--- enemy2.py
+++ enemy2.py
@@ -36,29 +36,10 @@ class Boss(Enemy):
         super().__init__(enemy_type, enemy_hp)
         self.boss_hp = boss_hp
         boss_health = enemy_hp + self.boss_hp
+        return boss_health
 
     def say_type(self):
         print("Boss {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
-
-class Megaboss(Enemy):
-    def __init__(self, enemy_type, enemy_hp, megaboss_hp, underlings=None):
-        super().__init__(enemy_type, enemy_hp)
-        if underlings == None:
-            self.underlings = []
-        else:
-            self.underlings = underlings
-    
-    def add_underling(self, underling):
-        if underling not in self.underlings:
-            self.underlings.append(underling)
-
-    def remove_underling(self, underling):
-        if underling in self.underlings:
-            self.underlings.remove(underling)
-
-    def print_underlings(self):
-        for underling in self.underlings:
-            print('Underling id', underling)
 
 
 e0 = Enemy('flying', 55)
