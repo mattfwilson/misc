9613f1324f9d8c9a40bd3ec1552902c8d03c7415
mattfwilson <mattfwilson@gmail.com>
2022-06-20 21:51:46 -0400

    Imported itertools for id incrementing

--- enemy_class.py
+++ enemy_class.py
@@ -1,27 +1,28 @@
+import itertools
+
 class Enemy:
-    id = 0
+    newid = itertools.count().__next__
 
     def __init__(self, enemy_type, enemy_hp):
+        self.newid = Enemy.newid()
         self.enemy_type = enemy_type
         self.enemy_hp = enemy_hp
-        self.id += 1
 
     @property
     def health(self):
-        """Docstring for the 'enemy_hp' property"""
-        return self.__enemy_hp
+        return self.enemy_hp
 
-    @health.settter
+    @health.setter
     def health(self, enemy_hp):
         if enemy_hp <= 10:
-            self.__enemy_hp = 10
+            self.enemy_hp = 10
     
     @health.deleter
     def health(self):
-        del self__enemy_hp
+        del self.enemy_hp
 
     def say_type(self):
-        print("Enemy {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
+        print("Enemy {} is type {} and has {} HP.".format(self.newid, self.enemy_type, self.enemy_hp))
 
 class Boss(Enemy):
 
@@ -31,7 +32,7 @@ class Boss(Enemy):
         boss_health = enemy_hp + self.boss_hp
 
     def say_type(self):
-        print("Boss {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp * 2))
+        print("Boss {} is type {} and has {} HP.".format(self.id, self.enemy_type, self.enemy_hp))
 
 class Megaboss(Enemy):
     def __init__(self, enemy_type, enemy_hp, megaboss_hp, underlings=None):
@@ -54,15 +55,10 @@ class Megaboss(Enemy):
             print('Underling id', underling.id)
 
 
-e1 = Enemy('Normal', 50)
-e1.say_type()
+e0 = Enemy('flying', 55)
+print(e0.say_type())
 
-e2 = Boss('Boss', 50, 4)
-e2.say_type()
-
-e3 = Megaboss('Ice', 50, 100)
-e3.add_underling(e2)
-e3.print_underlings()
-
+e1 = Enemy('water', 47)
+print(e1.say_type())
 
 
