78661f41689648095bf0f9b9d1e3b35cd065623a
mattfwilson <mattfwilson@gmail.com>
2022-06-23 00:05:51 -0400

    Fixed conditional repeating output within the while loop

--- rand_fun.py
+++ rand_fun.py
@@ -1,20 +1,17 @@
 import random
 
-# x = random.random()
-# x_length = len(x)
-
-# print(x_length)
-
 loop = True
 count = 0
 
 while loop is True:
     num = int(input('Enter a number: '))
 
-    if len(range(num)) > 3:
-        while (num > 0):
-            num = num // 10
-            count = count + 1
-        print('Total number of digits:', count)
-    else:
+    while (num > 0):
+        num = num // 10
+        count = count + 1
+    if count <= 2:
         print('Oh come on that\'s a puny number to count...')
+        count = 0
+    else:
+        print('Total number of digits:', count)
+        count = 0
