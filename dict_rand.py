15887ac803ce9d781a22bfc9c49fae5c22ef1d30
mattfwilson <mattfwilson@gmail.com>
2022-06-23 01:42:02 -0400

    Playing around with kwargs

--- dict_rand.py
+++ dict_rand.py
@@ -5,28 +5,27 @@ locations = {
         'Japanese': 7,
         'Mexican': 12,
         'Irish': 4,
-        'Chinese': 17,
-        'Lebanese': 2
     },
     'Seattle': {
         'Japanese': 2,
         'Mexican': 6,
         'Irish': 10,
-        'Chinese': 19,
-        'Lebanese': 5
     },
     'San Francisco': {
         'Japanese': 21,
         'Mexican': 32,
         'Irish': 11,
-        'Chinese': 103,
-        'Lebanese': 22
     },
 }
 
-rand_items = random.choice(list(locations.items()))
-rand_keys = random.choice(list(locations.keys()))
-rand_values = random.choice(list(locations.values()))
-print(rand_items)
-print(rand_keys)
-print(rand_values)
+def bestFood(**kwargs):
+    print(kwargs['city'] + ' has the best ' + kwargs['cuisine'] + ' food.')
+
+bestFood(city='New York City', cuisine='Lebanese')
+
+# rand_items = random.choice(list(locations.items()))
+# rand_keys = random.choice(list(locations.keys()))
+# rand_values = random.choice(list(locations.values()))
+# print(rand_items)
+# print(rand_keys)
+# print(rand_values)
