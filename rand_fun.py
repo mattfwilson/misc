8ffc7235af55e5f909e1cec6e1ea156c69cee51b
mattfwilson <mattfwilson@gmail.com>
2022-06-22 23:59:56 -0400

    Adding conditional

--- rand_fun.py
+++ rand_fun.py
@@ -8,11 +8,13 @@ import random
 loop = True
 count = 0
 
-while loop == True:
+while loop is True:
     num = int(input('Enter a number: '))
 
-    while (num > 0):
-        num = num // 10
-        count = count + 1
-
-    print('Total number of digits:', count)
+    if len(range(num)) > 3:
+        while (num > 0):
+            num = num // 10
+            count = count + 1
+        print('Total number of digits:', count)
+    else:
+        print('Oh come on that\'s a puny number to count...')
