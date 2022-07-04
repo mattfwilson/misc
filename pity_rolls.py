472152ae1c6ea66a1b5da096e3cc9bb68936b45a
mattfwilson <mattfwilson@gmail.com>
2022-07-04 17:48:54 -0400

    Changed some function/var names

--- pity_rolls.py
+++ pity_rolls.py
@@ -2,23 +2,23 @@ import random
 
 TOTAL_ROLLS = 0
 PITY_COUNTER = 0
-ITEM_POOL = []
-RARITY = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary']
+INVENTORY = []
+RARITY = ['Pebble', 'Silver Flake', 'Gold Nugget', 'Platinum Ore', 'Diamond']
 
 def roll():
     result = random.choices(RARITY, weights=[16, 12, 8, 4, 1])
-    ITEM_POOL.append(result[0])
-    print(f'You obtained a {result[0]} item!\n')
+    INVENTORY.append(result[0])
+    print(f'You obtained a {result[0]}!\n')
 
 def pity_roll():
     result = random.choices(RARITY, weights=[0, 0, 0, 0, 1])
-    ITEM_POOL.append(result[0])
-    print(f'PITY ROLL: You obtained a {result[0]} item!\n')
+    INVENTORY.append(result[0])
+    print(f'PITY ROLL: You obtained a {result[0]}!\n')
     return result[0]
 
-def inventory():
+def showInv():
     for i in RARITY:
-        print(f'{i}: {ITEM_POOL.count(i)}')
+        print(f'{i}: {INVENTORY.count(i)}')
 
 while True:
     option = input('What do you want to do? ')
@@ -32,6 +32,6 @@ while True:
             TOTAL_ROLLS += 1
             PITY_COUNTER += 1
     elif option == 'inv':
-        inventory()
+        showInv()
     elif option == 'count':
         print(f'You have rolled {TOTAL_ROLLS} times.')
