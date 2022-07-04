940171b9ef98bad97e629ab7a4ef022eb99c1a82
mattfwilson <mattfwilson@gmail.com>
2022-07-04 17:20:37 -0400

    Created a much more efficient function for displaying inventory

--- pity_rolls.py
+++ pity_rolls.py
@@ -1,39 +1,37 @@
 import random
-import time
 
-NUM_ROLLS = 0
-COUNTER = 10
-POOL = []
-RARITIES = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary']
+TOTAL_ROLLS = 0
+PITY_COUNTER = 0
+ITEM_POOL = []
+RARITY = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary']
 
 def roll():
-    result = random.choices(RARITIES, weights=[15, 12, 8, 4, 2])
-    POOL.append(result[0])
-    print(f'You obtained a {result[0]} item!')
-    return result[0]
+    result = random.choices(RARITY, weights=[16, 12, 8, 4, 1])
+    ITEM_POOL.append(result[0])
+    print(f'You obtained a {result[0]} item!\n')
 
 def pity_roll():
-    result = random.choices(RARITIES, weights=[0, 0, 0, 0, 1])
+    result = random.choices(RARITY, weights=[0, 0, 0, 0, 1])
+    ITEM_POOL.append(result[0])
+    print(f'PITY ROLL: You obtained a {result[0]} item!\n')
     return result[0]
 
-def summary():
-    common = POOL.count(RARITIES[0])
-    print(f'Common: {common}')
-    uncommon = POOL.count(RARITIES[1])
-    print(f'Uncommon: {uncommon}')
-    rare = POOL.count(RARITIES[2])
-    print(f'Rare: {rare}')
-    epic = POOL.count(RARITIES[3])
-    print(f'Epic: {epic}')
-    legendary = POOL.count(RARITIES[4])
-    print(f'Legendary: {legendary}')
+def inventory():
+    for i in RARITY:
+        print(f'{i}: {ITEM_POOL.count(i)}')
 
 while True:
-    option = input('What do you want to do? [roll/inv/count] ')
+    option = input('What do you want to do? ')
     if option == 'roll':
-        roll()
-        NUM_ROLLS += 1
+        if PITY_COUNTER == 10:
+            pity_roll()
+            TOTAL_ROLLS += 1
+            PITY_COUNTER = 0
+        else:
+            roll()
+            TOTAL_ROLLS += 1
+            PITY_COUNTER += 1
     elif option == 'inv':
-        summary()
+        inventory()
     elif option == 'count':
-        print(f'You have rolled {NUM_ROLLS} times.')
+        print(f'You have rolled {TOTAL_ROLLS} times.')
