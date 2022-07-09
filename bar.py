a773ed53c26ea3f54ae67fdcdf0c5e1c45c0c01b
mattfwilson <mattfwilson@gmail.com>
2022-07-08 21:34:44 -0400

    Fixed bug where user input could be nothing.

--- bar.py
+++ bar.py
@@ -7,16 +7,18 @@ AMOUNT = 0
 while True:
     print(f'\nHP: {CURRENT}/{MAX_BAR}')
     print('[' + BAR * CURRENT + SPACER * ' ' + ']')
-    AMOUNT = int(input('\nAdd or subtract from bar: '))
-
-    total = AMOUNT + CURRENT
-    if total > MAX_BAR:
-        print(f'Adding {AMOUNT} would exceed your max!')
-    elif total <= 0:
-        print(f'Subtracting {AMOUNT} would deplete your HP to 0!')
-    else:
-        CURRENT += AMOUNT
-        if AMOUNT < 0:
-            SPACER += abs(AMOUNT)
+    try:
+        AMOUNT = int(input('\nAdd or subtract from bar: '))
+        total = AMOUNT + CURRENT
+        if total > MAX_BAR:
+            print(f'Adding {AMOUNT} would exceed your max!')
+        elif total < 0:
+            print(f'Subtracting {AMOUNT} would deplete your HP to 0!')
         else:
-            SPACER -= abs(AMOUNT)
+            CURRENT += AMOUNT
+            if AMOUNT < 0:
+                SPACER += abs(AMOUNT)
+            else:
+                SPACER -= abs(AMOUNT)
+    except ValueError:
+        print('[!] You must input a number.')
